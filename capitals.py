cffd3cd3cebba5fa64cb6aea337b5fb7d24b10fa
jfw2855 <jwaddle1130@gmail.com>
2022-04-13 20:16:35 -0500

    deliverable complete

--- capitals.py
+++ capitals.py
@@ -1,3 +1,7 @@
+import math
+import random
+
+
 states = [
 {
     "name": "Alabama",
@@ -150,3 +154,45 @@ states = [
     "name": "Wyoming",
     "capital": "Cheyenne"
 }]
+
+
+import random
+
+#A State Capitals guessing game
+def play_game ():
+    player_score = {"right":0,"wrong":0}
+    random_numbers = []
+    state_questions = []
+    print("Welcome to State Capitals guessing game!")
+#generates the questions randomly without repeats
+    for i in range(50): # range(4) == [0,1,2,3]
+        gen_rand_num = random.randint(0,49)
+        while(gen_rand_num in random_numbers):
+            gen_rand_num = random.randint(0,49)
+        random_numbers.append(gen_rand_num)
+        state_questions.append(states[gen_rand_num])
+    for i in range(50):
+        state = state_questions[i]["name"]
+        capital = state_questions[i]["capital"]
+        guess = input(f"Question {i+1}, what is the capital of {state}? ")
+        if guess==capital:
+            player_score["right"]+=1
+            right = player_score["right"]
+            wrong = player_score["wrong"]
+            print(f"Correct! The answer is {capital}!\n Score: {right} Right , {wrong} Wrong")
+        else:
+            player_score["wrong"]+=1
+            right = player_score["right"]
+            wrong = player_score["wrong"]
+            print(f"Incorrect! The answer is {capital}!\n Score: {right} Right , {wrong} Wrong")
+    print("Game Over!")
+    #Checks if player would like to play again, if yes (y), then will recall play_game() [recursion]
+    replay = input("Would you like to play again? y or n ")
+    if (replay == "y"):
+        play_game()
+
+
+
+play_game()
+
+
