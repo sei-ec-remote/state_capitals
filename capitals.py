fd8aa671f135232c762e4cdeecadc05b46985c79
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-07-15 08:40:37 -0400

    game function working

--- capitals.py
+++ capitals.py
@@ -1,3 +1,6 @@
+import random
+from re import T
+
 states = [
 {
     "name": "Alabama",
@@ -8,145 +11,200 @@ states = [
 }, {
     "name": "Arizona",
     "capital": "Phoenix"
-}, {
-    "name": "Arkansas",
-    "capital": "Little Rock"
-}, {
-    "name": "California",
-    "capital": "Sacramento"
-}, {
-    "name": "Colorado",
-    "capital": "Denver"
-}, {
-    "name": "Connecticut",
-    "capital": "Hartford"
-}, {
-    "name": "Delaware",
-    "capital": "Dover"
-}, {
-    "name": "Florida",
-    "capital": "Tallahassee"
-}, {
-    "name": "Georgia",
-    "capital": "Atlanta"
-}, {
-    "name": "Hawaii",
-    "capital": "Honolulu"
-}, {
-    "name": "Idaho",
-    "capital": "Boise"
-}, {
-    "name": "Illinois",
-    "capital": "Springfield"
-}, {
-    "name": "Indiana",
-    "capital": "Indianapolis"
-}, {
-    "name": "Iowa",
-    "capital": "Des Moines"
-}, {
-    "name": "Kansas",
-    "capital": "Topeka"
-}, {
-    "name": "Kentucky",
-    "capital": "Frankfort"
-}, {
-    "name": "Louisiana",
-    "capital": "Baton Rouge"
-}, {
-    "name": "Maine",
-    "capital": "Augusta"
-}, {
-    "name": "Maryland",
-    "capital": "Annapolis"
-}, {
-    "name": "Massachusetts",
-    "capital": "Boston"
-}, {
-    "name": "Michigan",
-    "capital": "Lansing"
-}, {
-    "name": "Minnesota",
-    "capital": "St. Paul"
-}, {
-    "name": "Mississippi",
-    "capital": "Jackson"
-}, {
-    "name": "Missouri",
-    "capital": "Jefferson City"
-}, {
-    "name": "Montana",
-    "capital": "Helena"
-}, {
-    "name": "Nebraska",
-    "capital": "Lincoln"
-}, {
-    "name": "Nevada",
-    "capital": "Carson City"
-}, {
-    "name": "New Hampshire",
-    "capital": "Concord"
-}, {
-    "name": "New Jersey",
-    "capital": "Trenton"
-}, {
-    "name": "New Mexico",
-    "capital": "Santa Fe"
-}, {
-    "name": "New York",
-    "capital": "Albany"
-}, {
-    "name": "North Carolina",
-    "capital": "Raleigh"
-}, {
-    "name": "North Dakota",
-    "capital": "Bismarck"
-}, {
-    "name": "Ohio",
-    "capital": "Columbus"
-}, {
-    "name": "Oklahoma",
-    "capital": "Oklahoma City"
-}, {
-    "name": "Oregon",
-    "capital": "Salem"
-}, {
-    "name": "Pennsylvania",
-    "capital": "Harrisburg"
-}, {
-    "name": "Rhode Island",
-    "capital": "Providence"
-}, {
-    "name": "South Carolina",
-    "capital": "Columbia"
-}, {
-    "name": "South Dakota",
-    "capital": "Pierre"
-}, {
-    "name": "Tennessee",
-    "capital": "Nashville"
-}, {
-    "name": "Texas",
-    "capital": "Austin"
-}, {
-    "name": "Utah",
-    "capital": "Salt Lake City"
-}, {
-    "name": "Vermont",
-    "capital": "Montpelier"
-}, {
-    "name": "Virginia",
-    "capital": "Richmond"
-}, {
-    "name": "Washington",
-    "capital": "Olympia"
-}, {
-    "name": "West Virginia",
-    "capital": "Charleston"
-}, {
-    "name": "Wisconsin",
-    "capital": "Madison"
-}, {
-    "name": "Wyoming",
-    "capital": "Cheyenne"
-}]
+}
+# , {
+#     "name": "Arkansas",
+#     "capital": "Little Rock"
+# }, {
+#     "name": "California",
+#     "capital": "Sacramento"
+# }, {
+#     "name": "Colorado",
+#     "capital": "Denver"
+# }, {
+#     "name": "Connecticut",
+#     "capital": "Hartford"
+# }, {
+#     "name": "Delaware",
+#     "capital": "Dover"
+# }, {
+#     "name": "Florida",
+#     "capital": "Tallahassee"
+# }, {
+#     "name": "Georgia",
+#     "capital": "Atlanta"
+# }, {
+#     "name": "Hawaii",
+#     "capital": "Honolulu"
+# }, {
+#     "name": "Idaho",
+#     "capital": "Boise"
+# }, {
+#     "name": "Illinois",
+#     "capital": "Springfield"
+# }, {
+#     "name": "Indiana",
+#     "capital": "Indianapolis"
+# }, {
+#     "name": "Iowa",
+#     "capital": "Des Moines"
+# }, {
+#     "name": "Kansas",
+#     "capital": "Topeka"
+# }, {
+#     "name": "Kentucky",
+#     "capital": "Frankfort"
+# }, {
+#     "name": "Louisiana",
+#     "capital": "Baton Rouge"
+# }, {
+#     "name": "Maine",
+#     "capital": "Augusta"
+# }, {
+#     "name": "Maryland",
+#     "capital": "Annapolis"
+# }, {
+#     "name": "Massachusetts",
+#     "capital": "Boston"
+# }, {
+#     "name": "Michigan",
+#     "capital": "Lansing"
+# }, {
+#     "name": "Minnesota",
+#     "capital": "St. Paul"
+# }, {
+#     "name": "Mississippi",
+#     "capital": "Jackson"
+# }, {
+#     "name": "Missouri",
+#     "capital": "Jefferson City"
+# }, {
+#     "name": "Montana",
+#     "capital": "Helena"
+# }, {
+#     "name": "Nebraska",
+#     "capital": "Lincoln"
+# }, {
+#     "name": "Nevada",
+#     "capital": "Carson City"
+# }, {
+#     "name": "New Hampshire",
+#     "capital": "Concord"
+# }, {
+#     "name": "New Jersey",
+#     "capital": "Trenton"
+# }, {
+#     "name": "New Mexico",
+#     "capital": "Santa Fe"
+# }, {
+#     "name": "New York",
+#     "capital": "Albany"
+# }, {
+#     "name": "North Carolina",
+#     "capital": "Raleigh"
+# }, {
+#     "name": "North Dakota",
+#     "capital": "Bismarck"
+# }, {
+#     "name": "Ohio",
+#     "capital": "Columbus"
+# }, {
+#     "name": "Oklahoma",
+#     "capital": "Oklahoma City"
+# }, {
+#     "name": "Oregon",
+#     "capital": "Salem"
+# }, {
+#     "name": "Pennsylvania",
+#     "capital": "Harrisburg"
+# }, {
+#     "name": "Rhode Island",
+#     "capital": "Providence"
+# }, {
+#     "name": "South Carolina",
+#     "capital": "Columbia"
+# }, {
+#     "name": "South Dakota",
+#     "capital": "Pierre"
+# }, {
+#     "name": "Tennessee",
+#     "capital": "Nashville"
+# }, {
+#     "name": "Texas",
+#     "capital": "Austin"
+# }, {
+#     "name": "Utah",
+#     "capital": "Salt Lake City"
+# }, {
+#     "name": "Vermont",
+#     "capital": "Montpelier"
+# }, {
+#     "name": "Virginia",
+#     "capital": "Richmond"
+# }, {
+#     "name": "Washington",
+#     "capital": "Olympia"
+# }, {
+#     "name": "West Virginia",
+#     "capital": "Charleston"
+# }, {
+#     "name": "Wisconsin",
+#     "capital": "Madison"
+# }, {
+#     "name": "Wyoming",
+#     "capital": "Cheyenne"
+# }
+
+]
+
+# print("welcome to the capital quiz")
+# print("you will be asked 50 questions")
+# print("Please try to have correct answers as many as you can")
+
+
+class keys():
+   
+    def __init__(self,correct=0,incorrect=0,total=0):
+        self.correct = correct
+        self.incorrect = incorrect
+        self.total = total
+      
+    def right(self):
+        self.correct += 1
+        self.total += 1
+        return self.correct 
+    def wrong(self):
+        self.incorrect += 1
+        self.total += 1
+        return self.incorrect 
+    
+
+key = keys()
+
+def game():
+    random.shuffle(states)
+    # print(states)
+    for state in states:
+        print(f'what is the capital of {state["name"]}?')
+        answer = input()
+        if answer == state["capital"] or answer == state["capital"].lower():
+            key.right()
+            print(f'correct!')
+            print(f'{key.correct} correct answers out of {key.total}')
+        else:
+            key.wrong()
+            print(f'Incorrect! the capital is {state["capital"]}.')
+            print(f'{key.incorrect} wrong answers out of {key.total}')
+    if key.total == len(states):
+        print("Go or Stop")
+        a = input()
+        if a == "go":
+            key.total = 0
+            print("starting a new game now")
+            game()
+        else:
+            print("Thanks for playing")
+            exit()
+
+
+game()
