8e2313beb132bd986f87ec76fb5ab6b087b7c5b3
Mark Gerken <mgerken86@gmail.com>
2022-07-15 17:08:57 -0600

    Basic functionality of game

--- game.py
+++ game.py
@@ -5,7 +5,9 @@ import random
 # print(states)
 
 random_states = random.sample(states, len(states))
-print(random_states)
+# print(random_states)
+right_answers = 0
+wrong_answers = 0
 
 
 ## provide welcome message to introduce player to game
@@ -13,11 +15,30 @@ print(random_states)
 ## after all 50 states are guessed, user gets to decide whether to play again
 ## user can input 'quit' to exit game at any time
 ## add new keys to dictionary to track each time user gets specific capital right/wrong
+def play_game(right, wrong):
 
-while len(random_states) > 0:
-    ##randomly choose a state
-    # print('hello')
-    ##assign user input to value of capital
+    for state in random_states:
+        state['right'] = 0
+        state['wrong'] = 0
 
-    ##if capital is correct, 
+    print('Welcome to "State Capitals! You will be given a state, your job is to correctly guess the capital. A tally will be kept of your right and wrong answer counts. Good Luck!!')
+
+    while len(random_states) > 0:
+        state = random_states[0]
+        ##select first random state and assign user input to variable
+        user_guess = input(f"Guess the capital of ${state['name']}: ")
+        if user_guess == state['capital']:
+            right += 1
+            state['right'] += 1
+            print(f'Correct! ${user_guess} is the capital of ${state["name"]}')
+            print(f'You have guessed the capital of ${state["name"]} correctly ${state["right"]} times and incorrectly ${state["wrong"]} times')
+        else:
+            wrong -= 1
+            state['wrong'] -= 1
+            print(f'Incorrect. ${state["capital"]} is the capital of ${state["name"]}')
+
+        print(f'In total, you have guessed ${right} right answers and ${wrong} wrong answers.')
+        random_states.pop(0)
+
+play_game(right_answers, wrong_answers)
 
