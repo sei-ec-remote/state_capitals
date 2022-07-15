64d137b40b04e23479765e35de256056cae6f8d7
Mark Gerken <mgerken86@gmail.com>
2022-07-15 17:39:52 -0600

    Game fully works with tallies

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
 from capitals import states
 import random
-# from random import shuffle
 
-# print(states)
 
-random_states = random.sample(states, len(states))
-# print(random_states)
-right_answers = 0
-wrong_answers = 0
+right_ans = 0
+wrong_ans = 0
+# This creates keys for right / wrong answer tallies for each state
+for state in states:
+        state['right'] = 0
+        state['wrong'] = 0
 
 
 ## provide welcome message to introduce player to game
@@ -17,10 +17,8 @@ wrong_answers = 0
 ## add new keys to dictionary to track each time user gets specific capital right/wrong
 def play_game(right, wrong):
 
-    for state in random_states:
-        state['right'] = 0
-        state['wrong'] = 0
-
+    random_states = random.sample(states, len(states))
+    
     print('Welcome to "State Capitals! You will be given a state, your job is to correctly guess the capital. A tally will be kept of your right and wrong answer counts. Good Luck!!')
 
     while len(random_states) > 0:
@@ -31,14 +29,20 @@ def play_game(right, wrong):
             right += 1
             state['right'] += 1
             print(f'Correct! {user_guess} is the capital of {state["name"]}')
-            print(f'You have guessed the capital of {state["name"]} correctly {state["right"]} times and incorrectly {state["wrong"]} times')
         else:
-            wrong -= 1
+            wrong += 1
             state['wrong'] += 1
             print(f'Incorrect. {state["capital"]} is the capital of {state["name"]}')
-
+        print(f'You have guessed the capital of {state["name"]} correctly {state["right"]} times and incorrectly {state["wrong"]} times')
         print(f'In total, you have guessed {right} right answers and {wrong} wrong answers.')
         random_states.pop(0)
 
-play_game(right_answers, wrong_answers)
+    play_again = input('Enter "yes" to play again, any other character to quit: ').lower()
+    if play_again == 'yes':
+        random_states = states
+        play_game(right, wrong)
+    else:
+        print('Thanks for playing!!!')
+
+play_game(right_ans, wrong_ans)
 
