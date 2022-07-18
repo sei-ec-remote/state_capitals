62f292748a8f6ec5410c3ec3064a7436c5aeaa1b
kmcaloney15 <katymcaloney@gmail.com>
2022-07-18 18:54:22 -0400

    state capitals

--- capitals.py
+++ capitals.py
@@ -150,3 +150,76 @@ states = [
     "name": "Wyoming",
     "capital": "Cheyenne"
 }]
+
+test_states = [
+{
+    "name": "Virginia",
+    "capital": "Richmond"
+}, {
+    "name": "Washington",
+    "capital": "Olympia"
+}, {
+    "name": "West Virginia",
+    "capital": "Charleston"
+}, {
+    "name": "Wisconsin",
+    "capital": "Madison"
+}]
+
+
+#  --- IMPORTS --------------------------------------------
+
+import random
+
+
+#  ---- DEFINE FUNCTIONS -----------------------------------
+
+def State():
+    def __init__(self, name, capital):
+        self.name = name
+        self.capital = capital
+
+    
+# ---- MAIN -----------------------------------------------
+
+def game_function():
+
+# *Make sure the states don't appear in alphabetical order in the prompts. This will make the game a bit more challenging for the user.
+
+    random.shuffle(states)
+
+# *Provide a welcome message to introduce the player to the game.
+
+    print('Welcome to the State Capital Quiz!')
+
+# *Initialize new keys in the dictionaries that store the number of times a user gets a capital correct and the number of times the answer is wrong.
+
+    answers = 0
+
+# *Through all 50 states, prompt the user to name the capital of the state.
+
+    for state in states:
+        print('What is the capital of ' + state['name'] + '?')
+        answer = input()
+# *If the answer is correct, display a message saying so, and increment the correct key.
+        if answer == state['capital']:
+            answers += 1
+            print(f"Correct! You've gotten {answers}/50 right")
+# *If the answer is wrong, display a message saying so, and increment the wrong key.
+        else:
+            print('Incorrect!')
+            print(f"The correct answer is {state['capital']}. You've gotten {answers}/50 right")
+
+# *After each prompt, display a message telling the reader how many times the state was answered correctly out of the total number of times answered.
+
+# *Once the user has gone through all 50 states, ask them if they'd like to play again.
+    response = input('Would you like to play again? (y/n)')
+    if response == 'y':
+        game_function()
+    else:
+        print('Thanks for playing!')
+        exit()
+
+game_function()
+
+
