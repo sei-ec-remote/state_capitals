7248c199424221bca6e795c8ff0275e60a4f2bc3
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-08-11 17:27:31 -0400

    Added comments, fixed the placement of the randomizer. And made the actoually dict for the game.

--- capitals.py
+++ capitals.py
@@ -1,199 +1,295 @@
-# states = [
-# {
-#     "name": "Alabama",
-#     "capital": "Montgomery"
-# }, {
-#     "name": "Alaska",
-#     "capital": "Juneau"
-# }, {
-#     "name": "Arizona",
-#     "capital": "Phoenix"
-# }, {
-#     "name": "Arkansas",
-#     "capital": "Little Rock"
-# }, {
-#     "name": "California",
-#     "capital": "Sacramento"
-# }, {
-#     "name": "Colorado",
-#     "capital": "Denver"
-# }, {
-#     "name": "Connecticut",
-#     "capital": "Hartford"
-# }, {
-#     "name": "Delaware",
-#     "capital": "Dover"
-# }, {
-#     "name": "Florida",
-#     "capital": "Tallahassee"
-# }, {
-#     "name": "Georgia",
-#     "capital": "Atlanta"
-# }, {
-#     "name": "Hawaii",
-#     "capital": "Honolulu"
-# }, {
-#     "name": "Idaho",
-#     "capital": "Boise"
-# }, {
-#     "name": "Illinois",
-#     "capital": "Springfield"
-# }, {
-#     "name": "Indiana",
-#     "capital": "Indianapolis"
-# }, {
-#     "name": "Iowa",
-#     "capital": "Des Moines"
-# }, {
-#     "name": "Kansas",
-#     "capital": "Topeka"
-# }, {
-#     "name": "Kentucky",
-#     "capital": "Frankfort"
-# }, {
-#     "name": "Louisiana",
-#     "capital": "Baton Rouge"
-# }, {
-#     "name": "Maine",
-#     "capital": "Augusta"
-# }, {
-#     "name": "Maryland",
-#     "capital": "Annapolis"
-# }, {
-#     "name": "Massachusetts",
-#     "capital": "Boston"
-# }, {
-#     "name": "Michigan",
-#     "capital": "Lansing"
-# }, {
-#     "name": "Minnesota",
-#     "capital": "St. Paul"
-# }, {
-#     "name": "Mississippi",
-#     "capital": "Jackson"
-# }, {
-#     "name": "Missouri",
-#     "capital": "Jefferson City"
-# }, {
-#     "name": "Montana",
-#     "capital": "Helena"
-# }, {
-#     "name": "Nebraska",
-#     "capital": "Lincoln"
-# }, {
-#     "name": "Nevada",
-#     "capital": "Carson City"
-# }, {
-#     "name": "New Hampshire",
-#     "capital": "Concord"
-# }, {
-#     "name": "New Jersey",
-#     "capital": "Trenton"
-# }, {
-#     "name": "New Mexico",
-#     "capital": "Santa Fe"
-# }, {
-#     "name": "New York",
-#     "capital": "Albany"
-# }, {
-#     "name": "North Carolina",
-#     "capital": "Raleigh"
-# }, {
-#     "name": "North Dakota",
-#     "capital": "Bismarck"
-# }, {
-#     "name": "Ohio",
-#     "capital": "Columbus"
-# }, {
-#     "name": "Oklahoma",
-#     "capital": "Oklahoma City"
-# }, {
-#     "name": "Oregon",
-#     "capital": "Salem"
-# }, {
-#     "name": "Pennsylvania",
-#     "capital": "Harrisburg"
-# }, {
-#     "name": "Rhode Island",
-#     "capital": "Providence"
-# }, {
-#     "name": "South Carolina",
-#     "capital": "Columbia"
-# }, {
-#     "name": "South Dakota",
-#     "capital": "Pierre"
-# }, {
-#     "name": "Tennessee",
-#     "capital": "Nashville"
-# }, {
-#     "name": "Texas",
-#     "capital": "Austin"
-# }, {
-#     "name": "Utah",
-#     "capital": "Salt Lake City"
-# }, {
-#     "name": "Vermont",
-#     "capital": "Montpelier"
-# }, {
-#     "name": "Virginia",
-#     "capital": "Richmond"
-# }, {
-#     "name": "Washington",
-#     "capital": "Olympia"
-# }, {
-#     "name": "West Virginia",
-#     "capital": "Charleston"
-# }, {
-#     "name": "Wisconsin",
-#     "capital": "Madison"
-# }, {
-#     "name": "Wyoming",
-#     "capital": "Cheyenne"
-# }]
 states = [
 {
-    "name": "One",
-    "capital": "1",
+    "name": "Alabama",
+    "capital": "Montgomery",
     "correct": 0,
     "wrong": 0
 }, {
-    "name": "Two",
-    "capital": "2",
+    "name": "Alaska",
+    "capital": "Juneau",
     "correct": 0,
     "wrong": 0
 }, {
-    "name": "Three",
-    "capital": "3",
+    "name": "Arizona",
+    "capital": "Phoenix",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Arkansas",
+    "capital": "Little Rock",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "California",
+    "capital": "Sacramento",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Colorado",
+    "capital": "Denver",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Connecticut",
+    "capital": "Hartford",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Delaware",
+    "capital": "Dover",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Florida",
+    "capital": "Tallahassee",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Georgia",
+    "capital": "Atlanta",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Hawaii",
+    "capital": "Honolulu",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Idaho",
+    "capital": "Boise",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Illinois",
+    "capital": "Springfield",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Indiana",
+    "capital": "Indianapolis",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Iowa",
+    "capital": "Des Moines",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Kansas",
+    "capital": "Topeka",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Kentucky",
+    "capital": "Frankfort",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Louisiana",
+    "capital": "Baton Rouge",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Maine",
+    "capital": "Augusta",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Maryland",
+    "capital": "Annapolis",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Massachusetts",
+    "capital": "Boston",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Michigan",
+    "capital": "Lansing",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Minnesota",
+    "capital": "St. Paul",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Mississippi",
+    "capital": "Jackson",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Missouri",
+    "capital": "Jefferson City",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Montana",
+    "capital": "Helena",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Nebraska",
+    "capital": "Lincoln",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Nevada",
+    "capital": "Carson City",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "New Hampshire",
+    "capital": "Concord",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "New Jersey",
+    "capital": "Trenton",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "New Mexico",
+    "capital": "Santa Fe",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "New York",
+    "capital": "Albany",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "North Carolina",
+    "capital": "Raleigh",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "North Dakota",
+    "capital": "Bismarck",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Ohio",
+    "capital": "Columbus",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Oklahoma",
+    "capital": "Oklahoma City",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Oregon",
+    "capital": "Salem",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Pennsylvania",
+    "capital": "Harrisburg",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Rhode Island",
+    "capital": "Providence",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "South Carolina",
+    "capital": "Columbia",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "South Dakota",
+    "capital": "Pierre",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Tennessee",
+    "capital": "Nashville",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Texas",
+    "capital": "Austin",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Utah",
+    "capital": "Salt Lake City",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Vermont",
+    "capital": "Montpelier",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Virginia",
+    "capital": "Richmond",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Washington",
+    "capital": "Olympia",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "West Virginia",
+    "capital": "Charleston",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Wisconsin",
+    "capital": "Madison",
+    "correct": 0,
+    "wrong": 0
+}, {
+    "name": "Wyoming",
+    "capital": "Cheyenne",
     "correct": 0,
     "wrong": 0
 }]
 from ast import If
 import random
+# Keep the game going until user want's to quit
 playing = True
-round = 0
+# Tally for the correct and wrong count for the corrent round the user is playing. Resets after game has ended.
 correct_this_round = 0
 wrong_this_round = 0
-random.shuffle(states)
 
 # Provide a welcome message to introduce the player to the game.
 print("\nHello! Welcome to the game. We ask you questions, and you answer them. Then we tel you how smart you are. \n\nLets get started!!!!!!\n")
 
-while playing == True:        
+while playing == True:    
+    # randomize the list of questions.
+    random.shuffle(states)
+    # This loop is whre the round is played in.
     for state in states:
+        # ask the quetion.
         capital = input(f"What is the capital associated {state['name']}? ")
+        # Check the user's answer
         if capital == state['capital']:
+            # If user get's it right.
             print('\nYou got this correct you special snowflake you!\n')
             state['correct'] = state['correct'] + 1
             correct_this_round += 1
         else:
+            # If user get's it wrong.
             print('That wasn\'t right at all! How could you?\n')
             state['wrong'] = state['wrong'] + 1
             wrong_this_round += 1
+        # let the user know how they are doing compared to all rounds.
         print(f"This was answered correctly {state['correct']} out of the total of {state['correct'] + state['wrong']} times you have answered.")
+    # Let the user know how they did this round.
     print(f"You got {correct_this_round} of them correct!")
     print(f"You got {wrong_this_round} of them wrong!")
 
+    # Ask the user if they want to play another round.
     again = input('\nDo you want to play again? Maybe you can do better. (n/no): ')
+    # If the user says no do these things, if any other input the loop keeps going. 
+    # playing var true keeps the rounds going, so changed to false.
+    # Reset correct_this_round and wrong_this_round since they only track wrong and right for the round only.
     if again == 'n' or again == 'no':
         playing = False
     correct_this_round = 0
