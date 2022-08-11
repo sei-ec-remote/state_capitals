769ab2e6af1ac7ed758ec0c77dd1c71bf038ffd3
Alys <alice.g.cooper@gmail.com>
2022-08-11 16:34:51 -0400

    complete capital game quiz

--- capitals.py
+++ capitals.py
@@ -1,3 +1,5 @@
+import random
+
 states = [
 {
     "name": "Alabama",
@@ -150,3 +152,91 @@ states = [
     "name": "Wyoming",
     "capital": "Cheyenne"
 }]
+
+test_states = [
+{
+    "name": "Alabama",
+    "capital": "Montgomery"
+}, {
+    "name": "Alaska",
+    "capital": "Juneau"
+}, {
+    "name": "Arizona",
+    "capital": "Phoenix"
+}
+]
+
+player_score = {
+    "correct": 0,
+    "incorrect": 0
+}
+
+# let's create a game!
+# addm some new keys to dictionaries to store total number of right and wrong answers for players
+# player_score = {}
+
+########################################
+###### RIGHT, WRONG ANSWER FUNCTIONALITY
+########################################
+
+# after each prompt, display message telling player how many times the state was answered correctly (player_score[correct])
+
+def right_answer():
+    print("Congratulations, that was correct!")
+    player_score["correct"] = player_score["correct"] + 1
+    print(f'You have answered correctly a total of {player_score["correct"]} out of {player_score["correct"] + player_score["incorrect"]} times.')
+    return
+
+def wrong_answer():
+    print("I'm sorry, that was incorrect.")
+    player_score["incorrect"] = player_score["incorrect"] + 1
+    print(f'You have answered correctly a total of {player_score["correct"]} out of {player_score["correct"] + player_score["incorrect"]} times.')
+    return
+
+def play_again():
+    print('Congrats, you have gone through all the states!')
+    print(f'Your final score was {player_score["correct"]} out of {player_score["correct"] + player_score["incorrect"]}.')
+    play_again_answer = input(f'Would you like to play again? (yes, no)')
+    if play_again_answer == 'yes': 
+        correct = {'correct': 0}
+        incorrect = {'incorrect': 0}
+        player_score.update(correct)
+        player_score.update(incorrect)
+        # print(player_score)
+        play_game()
+    else:
+        print('Thank you for playing!')
+        return
+
+#######################
+###### GAME LOOP ######
+#######################
+
+# the game loop consists of:
+# the game will welcome the user to the game and introduce the rules
+welcome_message = 'Hello and welcome to guess the Capitol Cities! You will be given the name of a state, and your goal will be to answer with the correct state capitol name. Good luck!'
+
+# shuffle the dictionary to use first!
+
+def play_game():
+    random.shuffle(states)
+    for state in states:
+        # prompt the user to name the capitol of the state
+        answer = input(f'What is the capitol city of {state["name"]}? \n')
+        if answer == state["capital"]:
+            # if correct, provide 'right_answer' answer and increment the 'correct' key for player_score
+            right_answer()
+        else:
+            # if incorrect, provide 'wrong_answer' answer and increment the 'incorrect' key for player_score
+            wrong_answer()
+
+
+print(welcome_message)
+# nest this in a loop to do multiple times if wanted...
+if (player_score["correct"] + player_score["incorrect"]) < 50:
+    # looping once through all 50 states
+    play_game()
+if player_score["correct"] + player_score["incorrect"] == 50:
+    # once cycled through all 50 states, ask if they'd like to play again
+    play_again()
+
