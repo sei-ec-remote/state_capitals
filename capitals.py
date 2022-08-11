73be1b088f7db9b5214c2c21872197270550090d
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-08-11 09:57:07 -0400

    If the answer is correct, display a message saying so. If the answer is wrong, display a message saying so.

--- capitals.py
+++ capitals.py
@@ -182,14 +182,17 @@ while playing == True:
     for state in states:
         capital = input(f"What is the capital associated {state['name']}? ")
         if capital == state['capital']:
+            print('\nYou got this correct you special snowflake you!\n')
             state['correct'] = state['correct'] + 1
             correct_this_round += 1
         else:
+            print('That wasn\'t right at all! How could you?\n')
             state['wrong'] = state['wrong'] + 1
             wrong_this_round += 1
 
     print(f"You got {correct_this_round} of them correct!")
     print(f"You got {wrong_this_round} of them wrong!")
-    again = input('Do you want to play again? Maybe you can do better. (n/no)')
-    if again == 'n' or 'no':
+
+    again = input('\nDo you want to play again? Maybe you can do better. (n/no): ')
+    if again == 'n' or again == 'no':
         playing = False
