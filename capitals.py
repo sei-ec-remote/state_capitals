e98ddf3d16bb573c91c2d77d0ce2bbd589c7da26
jamestylladams <jamestylladams@gmail.com>
2022-08-11 20:45:17 -0600

    finsih deliverable

--- capitals.py
+++ capitals.py
@@ -1,3 +1,4 @@
+import random
 states = [
 {
     "name": "Alabama",
@@ -150,3 +151,41 @@ states = [
     "name": "Wyoming",
     "capital": "Cheyenne"
 }]
+correct = 0
+correct_states = []
+incorrect = 0
+incorrect_states = []
+turn = 0
+
+print("Welcome! Lets see how well you know US capital cities.")
+
+def quiz_capitals():
+    random.shuffle(states)
+    for state in states:
+        q = input(f'\n What is the capital of {state["name"]}? ')
+        global incorrect
+        global incorrect_states
+        global correct
+        global correct_states
+        global turn
+        if q == state["capital"]:
+            correct += 1
+            turn += 1
+            correct_states.append(state["name"][0:5:1])
+            print(f"\n Nice \n correct: {correct} \n{correct_states}, \n incorrect: {incorrect} \n {incorrect_states}")
+        else:
+            incorrect += 1
+            turn += 1
+            incorrect_states.append(state["name"][0:5:1].upper())
+            print(f"\n Nope! Its {state['capital']}. \n correct: {correct} \n {correct_states}, \n incorrect: {incorrect} \n {incorrect_states}")
+        
+        if turn > 50:
+            print('suhhh')
+
+quiz_capitals()
+post_q = input("Want to play again? ")
+
+if post_q == "yes":
+    quiz_capitals()
+else:
+    print('k bye')
