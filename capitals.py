3fb53744b1e92fe2875bdf48c60eabef151e0a4d
nickesparza <nickesparza@gmail.com>
2022-08-11 19:45:58 -0500

    add comments and refactor slightly

--- capitals.py
+++ capitals.py
@@ -169,11 +169,15 @@ capitals_stub = [
 
 class Question():
     def __init__(self, state):
+        # Question will hold a state dictionary and the history of right/wrong answers. It will also hold the game question about itself.
         self.state = state
         self.correct_history = 0
         self.wrong_history = 0
+    # quick string function to make it pretty
     def __str__(self):
         return f'this is a question pertaining to {self.state["capital"]}, {self.state["name"]}. It has been answered correctly {self.correct_history} times and incorrectly {self.wrong_history} times.'
+    # this function will be called for each question and will increment its answer history
+    # round scoring will be handles separately, since that will be reset more often
     def ask_the_question(self):
         answer = input(f'What is the capital of {self.state["name"]}?\n')
         if answer.lower() == self.state["capital"].lower():
@@ -183,19 +187,27 @@ class Question():
             self.wrong_history += 1
             return False
 
+# a function for sorting the questions by which ones have been answered incorrectly the most
 def sort_questions(question):
     return question.wrong_history
 
 def guessing_game():
+    # first, an empty array to hold the list
     question_list = []
-    for state in states:
+    # turn each state dictionary into an object that can ask a question
+    for state in capitals_stub:
         question_list.append(Question(state))
+    # shuffle this bad boy
     random.shuffle(question_list)
+    # set the number of rounds played so far
     rounds_played = 1
+    # this will be the trigger for the loop. If the player ever changes it to no, the program ends
     play_again = 'y'
     while play_again == 'y':
+        # set round scores here since they will be reset at the beginning of every loop
         correct_answers = 0
         wrong_answers = 0
+        # if this is the second or greater round, reshuffle the questions to have the wronger ones up higher
         if rounds_played > 1:
             question_list.sort(reverse=True, key=sort_questions)
         print("Welcome to Guess the Capitals!")
@@ -203,11 +215,13 @@ def guessing_game():
         print("Simply type the name of the capital of that state.")
         print("You'll see if you got it right immediately!")
         print(f'Round {rounds_played}! Let\'s go!')
+        # the loop to ask all the questions
         for question in question_list:
             ask = question.ask_the_question()
             if ask == True:
                 correct_answers += 1
                 print('Correct!')
+                # access that question's answer history to tell them how they're doing
                 print(f'You\'ve answered that question {question.correct_history} times correctly and {question.wrong_history} times incorrectly in the past.')
             else:
                 wrong_answers += 1
@@ -222,9 +236,7 @@ def guessing_game():
             continue
         elif play_again.lower() == 'n':
             print('Thanks for playing!')
-            return
         else:
-            print('Not sure what that means. Guess you\'re going again!')
-            continue
+            print('Not sure what that means. I\'ll let you go for now.')
 
 guessing_game()
