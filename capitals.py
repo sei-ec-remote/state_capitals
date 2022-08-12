ecb201f635051658f605c0689f0d42789478461b
cjones1047 <cjmj1047@gmail.com>
2022-08-11 22:47:41 -0500

    Trying to get game to be replayed on input

--- capitals.py
+++ capitals.py
@@ -1,3 +1,5 @@
+from random import*
+
 states = [
 {
     "name": "Alabama",
@@ -150,3 +152,94 @@ states = [
     "name": "Wyoming",
     "capital": "Cheyenne"
 }]
+
+short_state_list = [{
+    "name": "West Virginia",
+    "capital": "Charleston"
+}, {
+    "name": "Wisconsin",
+    "capital": "Madison"
+}, {
+    "name": "Wyoming",
+    "capital": "Cheyenne"
+}]
+
+class GameSession():
+    def __init__(self, questions_asked=0):
+        self.questions_asked = questions_asked
+        for state in short_state_list:
+            state['correct'] = 0
+            state['wrong'] = 0
+        # random.shuffle(short_state_list)
+        # print(randrange(0, len(short_state_list)))
+        # print(short_state_list)
+        print("Welcome to the State Capital Quiz Game!")
+        print("We will give you the name of a state and you try to guess its capital city.")
+        print("Good luck!")
+    def ask_question(self):
+        random_index = randrange(0, len(short_state_list))
+        answer = input(f"State capital of {short_state_list[random_index]['name']} is:\n")
+        if(answer == short_state_list[random_index]["capital"]):
+            print("Correct!")
+            short_state_list[random_index]['correct'] += 1
+        else: 
+            print("Wrong...")
+            short_state_list[random_index]['wrong'] += 1
+        correct = short_state_list[random_index]['correct']
+        total = short_state_list[random_index]['correct'] + short_state_list[random_index]['wrong']
+        print(f"You got the state of {short_state_list[random_index]['name']} right {correct}/{total} times")
+        self.questions_asked += 1
+        return
+    def number_of_questions(self):
+        return self.questions_asked
+
+
+
+this_game = GameSession()
+
+def play_game():
+    this_game.ask_question()
+    for state in short_state_list:
+        if(state['correct'] == 0 and state['wrong'] == 0):
+            play_game()
+    
+    # for i, state in enumerate(short_state_list):
+    #     if((state['correct'] + state['wrong']) == 0):
+    #         global number_of_states_quizzed_on += 1
+    #         continue
+    #     else:
+    #         global number_of_states_quizzed_on = i
+    #         break
+    # if(len(short_state_list) < this_game.number_of_questions()):
+    play_again = input("Want to play again?\n")
+
+    if(play_again.lower() == 'no'):
+        print("Thanks for playing!")
+    elif(play_again.lower() == 'yes'):
+        this_game
+        play_game()
+
+# if(number_of_states_quizzed_on < len(short_state_list)):
+#     play_game()
+play_game()
+
+    
+# print(len(short_state_list))
+# print(this_game.number_of_questions())
+
+
+# if(len(short_state_list) > number_of_states_quizzed_on):
+#     play_again = input("Want to play again?")
+#     if(play_again.lower() == 'no'):
+#         print("Thanks for playing!")
+#     else:
+#         this_game = GameSession()
+#         play_game()
+# else:
+#     print("keep on playing..")
+#     play_game()
+# random.shuffle(short_state_list)
+# this_game.ask_question()
+
+
+
