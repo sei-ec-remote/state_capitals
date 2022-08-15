bdfafad137ed5ec8db8d4ab529ee59cd0d4d7cb6
Zene09 <zeneorr@gmail.com>
2022-08-15 15:04:21 -0500

    changed states from list to a dictionary

--- capitals.py
+++ capitals.py
@@ -1,4 +1,4 @@
-states = [
+states = {
 {
     "name": "Alabama",
     "capital": "Montgomery"
@@ -149,4 +149,44 @@ states = [
 }, {
     "name": "Wyoming",
     "capital": "Cheyenne"
-}]
+}}
+
+# *Make sure the states don't appear in alphabetical order in the prompts. This will make the game a bit more challenging for the user.
+
+# *Initialize new keys in the dictionaries that store the number of times a user gets a capital correct and the number of times the answer is wrong.
+
+# *Through all 50 states, prompt the user to name the capital of the state.
+
+# *If the answer is correct, display a message saying so, and increment the correct key.
+
+# *If the answer is wrong, display a message saying so, and increment the wrong key.
+
+# *After each prompt, display a message telling the reader how many times the state was answered correctly out of the total number of times answered.
+
+# *Once the user has gone through all 50 states, ask them if they'd like to play again.
+
+# *Getting Started You're given an array of dictionaries that contain each state name and capital.
+
+# *Hint: For the purposes of developing this program, start with a test array of three dictionaries so you don't have to play through all 50 states each time.
+
+# Potentially Useful Methods *print *input *for loop *sorted *random.shuffle()
+
+# Bonus! Calculate a overall total score, display a running tally for each prompt If the user plays again, set the order of how the prompts appear to start with the ones they got wrong the most often. Add a hint functionality that prints the first 3 letters of a capital
+
+
+# The game needs a welcome message:
+print('Welcome! It\'s time to learn the state capitals of the United States.')
+
+# How many times did they get a capital wrong, store it here
+wrong_answers = 0
+
+# How many times did they answer correctly, store it here
+right_answers = 0
+
+# If they get an answer right:
+    # print(f"You answered correctly, the answer IS {states["capital"]}!")
+# If they get an answer wrong:
+    # print(f"That is the wrong answer, the correct answer is {states[capital]}.")
+# print (f"Correct Answers: {(self.)correct_answers} ; Wrong Answers: {wrong_answers})
+
+# print(len(states)) - states is acting as a list, not a dictionary?
