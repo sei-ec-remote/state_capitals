9c1ffcf022ce7e641bcaa13e44d2771b77065b79
danstacy1 <danstacy1@gmail.com>
2022-08-15 22:34:11 -0400

    completed delieverable

--- capitals.py
+++ capitals.py
@@ -1,3 +1,5 @@
+import random
+
 states = [
 {
     "name": "Alabama",
@@ -150,3 +152,76 @@ states = [
     "name": "Wyoming",
     "capital": "Cheyenne"
 }]
+
+# # test array
+# test_lists = [
+#     {
+#     "name": "Delaware",
+#     "capital": "Dover"
+# }, {
+#     "name": "Massachusetts",
+#     "capital": "Boston"
+# }, {
+#     "name": "Texas",
+#     "capital": "Austin"
+# }
+# ]
+
+for state in states:
+    state['right'] = 0
+    state['wrong'] = 0
+
+# print(test_list)
+
+# Welcome Message
+print("Welcome to the State Capitals Game! All answers must use proper capitalization.\n")
+play_game = True
+while (play_game):
+    random.shuffle(states)
+    # keeps track of how many answers are right and wrong
+    correct_answers = 0
+    incorrect_answers = 0
+    for state in states:
+        answer = input(f'What is the capital of {state["name"]}?\n')
+        # if the user gets the answer right
+        if answer == state['capital']:
+            # update correct answers for this round
+            correct_answers += 1
+            # update correct answers for this state in all rounds
+            state['right'] += 1
+            print("Good job! You got that one right!")
+        # if the user gets the answer wrong 
+        else:
+            # update incorrect answers for this round
+            incorrect_answers += 1
+            # update incorrect answers for this state in all rounds
+            state['wrong'] += 1
+            print("Sorry, that is not correct.")
+        # display how many correct and incorrect answers for this round
+        print(f"Correct Answers: {correct_answers} | Incorrect Answers {incorrect_answers}\n")
+        # display how many correct and incorrect answers for each state for all rounds
+        print(f"Correct Answers For This State: {state['right']} | Incorrect Answers For This State {state['wrong']}\n")
+    # Ask the user if they want to play again.
+    restart_game = input('Do you want to play again? (yes/no)\n')
+    if restart_game == 'no':
+        play_game = False
+        print('Thanks for playing! Come back soon!')
+    if restart_game == 'yes':
+        play_game = True
+
+
+# this randomizes the list
+# this prints the randomized list
+# print(test_list)
+
+# ask the user to what the state capital of a given state is
+
+
+
+
+# if the user answers the question correctly/incorrectly, display a message that says so
+
+# keep score of how many the user has answered correct and incorrect
+
+# after the game is over, ask if the user wants to play again
+
