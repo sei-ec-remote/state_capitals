d3f78235f43bb626e8d02b5d4925b0592ac72228
nabbott98 <nicholasabbott98@gmail.com>
2022-11-14 20:22:37 -0500

    Capitals game complete

--- capitals.py
+++ capitals.py
@@ -1,152 +1,208 @@
+# Imports
+import random
+#import webbrowser
+
+# states = [
+# {
+#     "name": "Alabama",
+#     "capital": "Montgomery"
+# }, {
+#     "name": "Alaska",
+#     "capital": "Juneau"
+# }, {
+#     "name": "Arizona",
+#     "capital": "Phoenix"
+# }, {
+#     "name": "Arkansas",
+#     "capital": "Little Rock"
+# }, {
+#     "name": "California",
+#     "capital": "Sacramento"
+# }, {
+#     "name": "Colorado",
+#     "capital": "Denver"
+# }, {
+#     "name": "Connecticut",
+#     "capital": "Hartford"
+# }, {
+#     "name": "Delaware",
+#     "capital": "Dover"
+# }, {
+#     "name": "Florida",
+#     "capital": "Tallahassee"
+# }, {
+#     "name": "Georgia",
+#     "capital": "Atlanta"
+# }, {
+#     "name": "Hawaii",
+#     "capital": "Honolulu"
+# }, {
+#     "name": "Idaho",
+#     "capital": "Boise"
+# }, {
+#     "name": "Illinois",
+#     "capital": "Springfield"
+# }, {
+#     "name": "Indiana",
+#     "capital": "Indianapolis"
+# }, {
+#     "name": "Iowa",
+#     "capital": "Des Moines"
+# }, {
+#     "name": "Kansas",
+#     "capital": "Topeka"
+# }, {
+#     "name": "Kentucky",
+#     "capital": "Frankfort"
+# }, {
+#     "name": "Louisiana",
+#     "capital": "Baton Rouge"
+# }, {
+#     "name": "Maine",
+#     "capital": "Augusta"
+# }, {
+#     "name": "Maryland",
+#     "capital": "Annapolis"
+# }, {
+#     "name": "Massachusetts",
+#     "capital": "Boston"
+# }, {
+#     "name": "Michigan",
+#     "capital": "Lansing"
+# }, {
+#     "name": "Minnesota",
+#     "capital": "St. Paul"
+# }, {
+#     "name": "Mississippi",
+#     "capital": "Jackson"
+# }, {
+#     "name": "Missouri",
+#     "capital": "Jefferson City"
+# }, {
+#     "name": "Montana",
+#     "capital": "Helena"
+# }, {
+#     "name": "Nebraska",
+#     "capital": "Lincoln"
+# }, {
+#     "name": "Nevada",
+#     "capital": "Carson City"
+# }, {
+#     "name": "New Hampshire",
+#     "capital": "Concord"
+# }, {
+#     "name": "New Jersey",
+#     "capital": "Trenton"
+# }, {
+#     "name": "New Mexico",
+#     "capital": "Santa Fe"
+# }, {
+#     "name": "New York",
+#     "capital": "Albany"
+# }, {
+#     "name": "North Carolina",
+#     "capital": "Raleigh"
+# }, {
+#     "name": "North Dakota",
+#     "capital": "Bismarck"
+# }, {
+#     "name": "Ohio",
+#     "capital": "Columbus"
+# }, {
+#     "name": "Oklahoma",
+#     "capital": "Oklahoma City"
+# }, {
+#     "name": "Oregon",
+#     "capital": "Salem"
+# }, {
+#     "name": "Pennsylvania",
+#     "capital": "Harrisburg"
+# }, {
+#     "name": "Rhode Island",
+#     "capital": "Providence"
+# }, {
+#     "name": "South Carolina",
+#     "capital": "Columbia"
+# }, {
+#     "name": "South Dakota",
+#     "capital": "Pierre"
+# }, {
+#     "name": "Tennessee",
+#     "capital": "Nashville"
+# }, {
+#     "name": "Texas",
+#     "capital": "Austin"
+# }, {
+#     "name": "Utah",
+#     "capital": "Salt Lake City"
+# }, {
+#     "name": "Vermont",
+#     "capital": "Montpelier"
+# }, {
+#     "name": "Virginia",
+#     "capital": "Richmond"
+# }, {
+#     "name": "Washington",
+#     "capital": "Olympia"
+# }, {
+#     "name": "West Virginia",
+#     "capital": "Charleston"
+# }, {
+#     "name": "Wisconsin",
+#     "capital": "Madison"
+# }, {
+#     "name": "Wyoming",
+#     "capital": "Cheyenne"
+# }]
+
 states = [
 {
     "name": "Alabama",
-    "capital": "Montgomery"
+    "capital": "M"
 }, {
     "name": "Alaska",
-    "capital": "Juneau"
+    "capital": "J"
 }, {
     "name": "Arizona",
-    "capital": "Phoenix"
-}, {
-    "name": "Arkansas",
-    "capital": "Little Rock"
-}, {
-    "name": "California",
-    "capital": "Sacramento"
-}, {
-    "name": "Colorado",
-    "capital": "Denver"
-}, {
-    "name": "Connecticut",
-    "capital": "Hartford"
-}, {
-    "name": "Delaware",
-    "capital": "Dover"
-}, {
-    "name": "Florida",
-    "capital": "Tallahassee"
-}, {
-    "name": "Georgia",
-    "capital": "Atlanta"
-}, {
-    "name": "Hawaii",
-    "capital": "Honolulu"
-}, {
-    "name": "Idaho",
-    "capital": "Boise"
-}, {
-    "name": "Illinois",
-    "capital": "Springfield"
-}, {
-    "name": "Indiana",
-    "capital": "Indianapolis"
-}, {
-    "name": "Iowa",
-    "capital": "Des Moines"
-}, {
-    "name": "Kansas",
-    "capital": "Topeka"
-}, {
-    "name": "Kentucky",
-    "capital": "Frankfort"
-}, {
-    "name": "Louisiana",
-    "capital": "Baton Rouge"
-}, {
-    "name": "Maine",
-    "capital": "Augusta"
-}, {
-    "name": "Maryland",
-    "capital": "Annapolis"
-}, {
-    "name": "Massachusetts",
-    "capital": "Boston"
-}, {
-    "name": "Michigan",
-    "capital": "Lansing"
-}, {
-    "name": "Minnesota",
-    "capital": "St. Paul"
-}, {
-    "name": "Mississippi",
-    "capital": "Jackson"
-}, {
-    "name": "Missouri",
-    "capital": "Jefferson City"
-}, {
-    "name": "Montana",
-    "capital": "Helena"
-}, {
-    "name": "Nebraska",
-    "capital": "Lincoln"
-}, {
-    "name": "Nevada",
-    "capital": "Carson City"
-}, {
-    "name": "New Hampshire",
-    "capital": "Concord"
-}, {
-    "name": "New Jersey",
-    "capital": "Trenton"
-}, {
-    "name": "New Mexico",
-    "capital": "Santa Fe"
-}, {
-    "name": "New York",
-    "capital": "Albany"
-}, {
-    "name": "North Carolina",
-    "capital": "Raleigh"
-}, {
-    "name": "North Dakota",
-    "capital": "Bismarck"
-}, {
-    "name": "Ohio",
-    "capital": "Columbus"
-}, {
-    "name": "Oklahoma",
-    "capital": "Oklahoma City"
-}, {
-    "name": "Oregon",
-    "capital": "Salem"
-}, {
-    "name": "Pennsylvania",
-    "capital": "Harrisburg"
-}, {
-    "name": "Rhode Island",
-    "capital": "Providence"
-}, {
-    "name": "South Carolina",
-    "capital": "Columbia"
-}, {
-    "name": "South Dakota",
-    "capital": "Pierre"
-}, {
-    "name": "Tennessee",
-    "capital": "Nashville"
-}, {
-    "name": "Texas",
-    "capital": "Austin"
-}, {
-    "name": "Utah",
-    "capital": "Salt Lake City"
-}, {
-    "name": "Vermont",
-    "capital": "Montpelier"
-}, {
-    "name": "Virginia",
-    "capital": "Richmond"
-}, {
-    "name": "Washington",
-    "capital": "Olympia"
-}, {
-    "name": "West Virginia",
-    "capital": "Charleston"
-}, {
-    "name": "Wisconsin",
-    "capital": "Madison"
-}, {
-    "name": "Wyoming",
-    "capital": "Cheyenne"
+    "capital": "P"
 }]
+
+# Add record at start to help with global score tracking
+for state in states:
+    state["correct"] = 0
+    state["incorrect"] = 0
+
+
+def capitals_game(states):
+    print('The game has started')
+    random.shuffle(states)
+
+    correct = 0
+
+    # Game Loop
+    for state in states:
+        print('The capital of', state["name"], 'is: ', end ='')
+        if input().casefold() == state["capital"].casefold():
+            correct += 1
+            state["correct"] += 1
+            print('Correct! +1', end='   ')
+        else:
+            state["incorrect"] += 1
+            print('incorrect', end='   ')
+        print(state["correct"], '/', state["correct"] + state["incorrect"])
+    
+    print('You scored a', correct, 'out of', len(states), 'this round')
+
+    # Game over play again?
+    print('Would you like to play again? (Y): ', end='')
+    if input().casefold() == 'y':
+        print('answered yes')
+        capitals_game(states)
+
+
+print('Welcome to the State Capitals Game!')
+print('Identify the capital associated with a given state, and complete the statement!')
+#print('Time to play the (State Capitals) game hahahahaha *Triple H slams through your door and spits water*')
+# webbrowser.open_new_tab('https://youtu.be/P0gqB1bVK3g?t=84')
+
+capitals_game(states)
