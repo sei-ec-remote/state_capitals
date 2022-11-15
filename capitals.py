cbbc59149a0b35437e2a469f4086d1b5e47aea3b
iceagelemonade <bryanjcarr88@gmail.com>
2022-11-14 21:57:10 -0500

    complete

--- capitals.py
+++ capitals.py
@@ -150,3 +150,39 @@ states = [
     "name": "Wyoming",
     "capital": "Cheyenne"
 }]
+
+short_list = [
+{
+    "name": "New York",
+    "capital": "Albany"
+},{
+    "name": "Delaware",
+    "capital": "Dover"
+}]
+
+score = {"right" : 0, "wrong" : 0 }
+def game(states):
+    import random
+    import math
+    random.shuffle(states)
+    print("Let's play a game! Let's see how many state capitals you know!")
+    
+    for pair in states:
+        print('What is the capital of',pair["name"],'?')
+        answer = input('Answer: ')
+        if answer.lower() == pair["capital"].lower():
+            print("That's right! Good job!")
+            score["right"] += 1
+        else :
+            print("Sorry, the answer was",pair["capital"],".")
+            score["wrong"] += 1
+        print("You have gotten",math.floor((score["right"] / (score["right"] + score["wrong"])) * 100), "% correct so far (",score["right"],"/",score["right"] + score["wrong"],").")
+
+    print("That is all 50 states! You scored:",math.floor((score["right"] / (score["right"] + score["wrong"])) * 100))
+    again = input("Would you like to play again? (yes/no)")
+    if again.lower() == "yes":
+        game(states)
+    else:
+        print("Thanks for playing! Goodbye!")
+
+game(short_list)
