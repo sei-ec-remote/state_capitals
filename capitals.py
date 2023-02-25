428680da8405c4ac10ac8f9ea9bb42b853ed5130
thepjos <josh.thephasdin@gmail.com>
2023-02-25 09:30:16 -0600

    game done

--- capitals.py
+++ capitals.py
@@ -19,178 +19,190 @@ states = [
     "capital": "Denver"
 }, {
     "name": "Connecticut",
-    "capital": "Hartford"}]
-# }, {
-#     "name": "Delaware",
-#     "capital": "Dover"
-# }, {
-#     "name": "Florida",
-#     "capital": "Tallahassee"
-# }, {
-#     "name": "Georgia",
-#     "capital": "Atlanta"
-# }, {
-#     "name": "Hawaii",
-#     "capital": "Honolulu"
-# }, {
-#     "name": "Idaho",
-#     "capital": "Boise"
-# }, {
-#     "name": "Illinois",
-#     "capital": "Springfield"
-# }, {
-#     "name": "Indiana",
-#     "capital": "Indianapolis"
-# }, {
-#     "name": "Iowa",
-#     "capital": "Des Moines"
-# }, {
-#     "name": "Kansas",
-#     "capital": "Topeka"
-# }, {
-#     "name": "Kentucky",
-#     "capital": "Frankfort"
-# }, {
-#     "name": "Louisiana",
-#     "capital": "Baton Rouge"
-# }, {
-#     "name": "Maine",
-#     "capital": "Augusta"
-# }, {
-#     "name": "Maryland",
-#     "capital": "Annapolis"
-# }, {
-#     "name": "Massachusetts",
-#     "capital": "Boston"
-# }, {
-#     "name": "Michigan",
-#     "capital": "Lansing"
-# }, {
-#     "name": "Minnesota",
-#     "capital": "St. Paul"
-# }, {
-#     "name": "Mississippi",
-#     "capital": "Jackson"
-# }, {
-#     "name": "Missouri",
-#     "capital": "Jefferson City"
-# }, {
-#     "name": "Montana",
-#     "capital": "Helena"
-# }, {
-#     "name": "Nebraska",
-#     "capital": "Lincoln"
-# }, {
-#     "name": "Nevada",
-#     "capital": "Carson City"
-# }, {
-#     "name": "New Hampshire",
-#     "capital": "Concord"
-# }, {
-#     "name": "New Jersey",
-#     "capital": "Trenton"
-# }, {
-#     "name": "New Mexico",
-#     "capital": "Santa Fe"
-# }, {
-#     "name": "New York",
-#     "capital": "Albany"
-# }, {
-#     "name": "North Carolina",
-#     "capital": "Raleigh"
-# }, {
-#     "name": "North Dakota",
-#     "capital": "Bismarck"
-# }, {
-#     "name": "Ohio",
-#     "capital": "Columbus"
-# }, {
-#     "name": "Oklahoma",
-#     "capital": "Oklahoma City"
-# }, {
-#     "name": "Oregon",
-#     "capital": "Salem"
-# }, {
-#     "name": "Pennsylvania",
-#     "capital": "Harrisburg"
-# }, {
-#     "name": "Rhode Island",
-#     "capital": "Providence"
-# }, {
-#     "name": "South Carolina",
-#     "capital": "Columbia"
-# }, {
-#     "name": "South Dakota",
-#     "capital": "Pierre"
-# }, {
-#     "name": "Tennessee",
-#     "capital": "Nashville"
-# }, {
-#     "name": "Texas",
-#     "capital": "Austin"
-# }, {
-#     "name": "Utah",
-#     "capital": "Salt Lake City"
-# }, {
-#     "name": "Vermont",
-#     "capital": "Montpelier"
-# }, {
-#     "name": "Virginia",
-#     "capital": "Richmond"
-# }, {
-#     "name": "Washington",
-#     "capital": "Olympia"
-# }, {
-#     "name": "West Virginia",
-#     "capital": "Charleston"
-# }, {
-#     "name": "Wisconsin",
-#     "capital": "Madison"
-# }, {
-#     "name": "Wyoming",
-#     "capital": "Cheyenne"
-# }]
+    "capital": "Hartford"
+}, {
+    "name": "Delaware",
+    "capital": "Dover"
+}, {
+    "name": "Florida",
+    "capital": "Tallahassee"
+}, {
+    "name": "Georgia",
+    "capital": "Atlanta"
+}, {
+    "name": "Hawaii",
+    "capital": "Honolulu"
+}, {
+    "name": "Idaho",
+    "capital": "Boise"
+}, {
+    "name": "Illinois",
+    "capital": "Springfield"
+}, {
+    "name": "Indiana",
+    "capital": "Indianapolis"
+}, {
+    "name": "Iowa",
+    "capital": "Des Moines"
+}, {
+    "name": "Kansas",
+    "capital": "Topeka"
+}, {
+    "name": "Kentucky",
+    "capital": "Frankfort"
+}, {
+    "name": "Louisiana",
+    "capital": "Baton Rouge"
+}, {
+    "name": "Maine",
+    "capital": "Augusta"
+}, {
+    "name": "Maryland",
+    "capital": "Annapolis"
+}, {
+    "name": "Massachusetts",
+    "capital": "Boston"
+}, {
+    "name": "Michigan",
+    "capital": "Lansing"
+}, {
+    "name": "Minnesota",
+    "capital": "St. Paul"
+}, {
+    "name": "Mississippi",
+    "capital": "Jackson"
+}, {
+    "name": "Missouri",
+    "capital": "Jefferson City"
+}, {
+    "name": "Montana",
+    "capital": "Helena"
+}, {
+    "name": "Nebraska",
+    "capital": "Lincoln"
+}, {
+    "name": "Nevada",
+    "capital": "Carson City"
+}, {
+    "name": "New Hampshire",
+    "capital": "Concord"
+}, {
+    "name": "New Jersey",
+    "capital": "Trenton"
+}, {
+    "name": "New Mexico",
+    "capital": "Santa Fe"
+}, {
+    "name": "New York",
+    "capital": "Albany"
+}, {
+    "name": "North Carolina",
+    "capital": "Raleigh"
+}, {
+    "name": "North Dakota",
+    "capital": "Bismarck"
+}, {
+    "name": "Ohio",
+    "capital": "Columbus"
+}, {
+    "name": "Oklahoma",
+    "capital": "Oklahoma City"
+}, {
+    "name": "Oregon",
+    "capital": "Salem"
+}, {
+    "name": "Pennsylvania",
+    "capital": "Harrisburg"
+}, {
+    "name": "Rhode Island",
+    "capital": "Providence"
+}, {
+    "name": "South Carolina",
+    "capital": "Columbia"
+}, {
+    "name": "South Dakota",
+    "capital": "Pierre"
+}, {
+    "name": "Tennessee",
+    "capital": "Nashville"
+}, {
+    "name": "Texas",
+    "capital": "Austin"
+}, {
+    "name": "Utah",
+    "capital": "Salt Lake City"
+}, {
+    "name": "Vermont",
+    "capital": "Montpelier"
+}, {
+    "name": "Virginia",
+    "capital": "Richmond"
+}, {
+    "name": "Washington",
+    "capital": "Olympia"
+}, {
+    "name": "West Virginia",
+    "capital": "Charleston"
+}, {
+    "name": "Wisconsin",
+    "capital": "Madison"
+}, {
+    "name": "Wyoming",
+    "capital": "Cheyenne"
+}]
 
-# start here
-# example data
-# states = [
-# {
-#     "name": "Alabama",
-#     "capital": "Montgomery"
-# },
+
 
 import random
 
-print("Welcome to the state capital game.")
-print("The object of the game is to guess the correct capital name for each state.")
-print("For each wrong answer, you will get one incorrect point. For each correct answer, you will get one correct point.")
+def play_capitals():
 
-correct = 0
-incorrect = 0
 
-for i in range(len(states)):
+    print("Welcome to the state capital game.")
+    print("The object of the game is to guess the correct capital name for each state.")
+    print("For each wrong answer, you will get one incorrect point. For each correct answer, you will get one correct point.")
 
-    state_choice = states[i]['name']
-    choice = input(f"What is the capital of {state_choice} ? " ).title()
+    correct = 0
+    incorrect = 0
+    s_states = random.sample(states, len(states))
 
-    print(f'You chose {choice}')
+    for i in range(len(states)):
 
-    capital = states[i]['capital']
-    while True:
-        if choice == states[i]['capital']:
-            print(f'Correct, the capital is {choice}')
-            correct += 1
-            print(f'Your scores are {correct} correct and {incorrect} incorrect')
-            break
-        else:
-            print(f'Sorry, the capital of {state_choice} is not {choice}. ')
-            incorrect += 1
-            print(f'Your scores are {correct} correct and {incorrect} incorrect')
-            
-            print('Please try again.')
-            choice = input(f"What is the capital of {state_choice} ? " ).capitalize()
+        state_choice = s_states[i]['name']
+        choice = input(f"What is the capital of {state_choice} ? " ).title()
 
-            
+        print(f'You chose {choice}')
+
+        capital = s_states[i]['capital']
+        while True:
+            if choice == (s_states[i]['capital'].title()):
+                print(f'Correct, the capital is {choice}')
+                correct += 1
+                print(f'Your scores are {correct} correct and {incorrect} incorrect')
+                break
+            else:
+                print(f'Sorry, the capital of {state_choice} is not {choice}. ')
+                incorrect += 1
+                print(f'Your scores are {correct} correct and {incorrect} incorrect')
+                
+                print('Please try again.')
+                choice = input(f"What is the capital of {state_choice} ? " ).title()
+
+
+while True:
+    play_capitals()
+    
+    restart = input('Do you want to play again Y/N?')
+    if restart == 'N':
+        print('Thanks for playin')
+        break
+    elif restart == 'Y':
+        print('Playing again! ')
+        continue
+
+
+
+
 
 
 # print(f'You chose {choice}. The correct choice is {capital}')
