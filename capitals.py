c5d0340dd7a581ffbb78fdd28ed0fcbf73d3af8e
danieljo <hyundanieljo@gmail.com>
2023-02-26 20:31:10 -0500

    Completed deliverable

--- capitals.py
+++ capitals.py
@@ -150,3 +150,41 @@ states = [
     "name": "Wyoming",
     "capital": "Cheyenne"
 }]
+
+import random
+
+def caps_game(states):
+    print("Welcome to the Capital Game! Can you name the capital of each state?")
+    random.shuffle(states)
+
+    for state in states:
+        state["correct"] = 0
+        state["incorrect"] = 0
+
+    total_correct = 0
+    total_incorrect = 0
+
+    for state in states:
+        print(f"What is the capital of {state['name']}?")
+        answer = input().lower()
+        if answer == state["capital"].lower():
+            state["correct"] += 1
+            total_correct += 1
+            print("Correct!")
+        else:
+            state["incorrect"] += 1
+            total_incorrect += 1
+            print(f"Incorrect. The capital of {state['name']} is {state['capital']}.")
+
+        print(f"Correct Answers {total_correct} Incorrect answers {total_incorrect}")
+
+    print(f"You answered {total_correct} states correctly and {total_incorrect} states incorrectly.")
+
+    play_again = input("You have finished all 50 states. Would you like to play again? (yes/no)").lower()
+
+    if play_again == "yes":
+        caps_game(states)
+    else:
+        print("Thanks for playing!")
+
+caps_game(states)
