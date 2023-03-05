1b3d898b0c75ba6d4fef54ea9bcdc008bbc9396a
KenGibb <kenjibbo@gmail.com>
2023-03-05 14:04:26 -0600

    state capitals complete

--- capitals.py
+++ capitals.py
@@ -1,3 +1,5 @@
+import random
+
 states = [
 {
     "name": "Alabama",
@@ -150,3 +152,39 @@ states = [
     "name": "Wyoming",
     "capital": "Cheyenne"
 }]
+
+def capital_game(states):
+
+    print("Welcome to the Capital Game! Can you name the capital of each state?")
+    random.shuffle(states)
+
+    for state in states:
+        state["correct"] = 0
+        state["wrong"] = 0
+
+    total_correct = 0
+    total_wrong = 0
+    
+    for state in states:
+        print("What is the capital of " + state["name"] + "?")
+        answer = input()
+        if answer.lower() == state["capital"].lower():
+            print("Correct!")
+            state["correct"] += 1
+            total_correct += 1
+        else:
+            print("Incorrect. The capital of " + state["name"] + " is " + state["capital"] + ".")
+            state["wrong"] += 1
+            total_wrong += 1
+        print("Correct Answers " + str(total_correct) + " Incorrect answers " + str(total_wrong))
+        
+    print("You answered " + str(total_correct) + " states correctly and " + str(total_wrong) + " states incorrectly.")
+    
+    play_again = input("You have finished all 50 states. Would you like to play again? (y/n)")
+    
+    if play_again.lower() == "y":
+        capital_game(states)
+    else:
+        print("Thanks for playing!")
+
+capital_game(states)
