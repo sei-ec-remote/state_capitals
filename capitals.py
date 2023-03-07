aef4da3a347cdfafb8ca893cfbce32a00397bd1b
bruceabrigo <bruceabrigo@outlook.com>
2023-03-07 10:42:12 -0600

    solved

--- capitals.py
+++ capitals.py
@@ -1,3 +1,6 @@
+import random
+# import random at the top, this will allow use to use the random.shuffle() func on the states list
+
 states = [
 {
     "name": "Alabama",
@@ -5,148 +8,50 @@ states = [
 }, {
     "name": "Alaska",
     "capital": "Juneau"
-}, {
-    "name": "Arizona",
-    "capital": "Phoenix"
-}, {
-    "name": "Arkansas",
-    "capital": "Little Rock"
-}, {
-    "name": "California",
-    "capital": "Sacramento"
-}, {
-    "name": "Colorado",
-    "capital": "Denver"
-}, {
-    "name": "Connecticut",
-    "capital": "Hartford"
-}, {
-    "name": "Delaware",
-    "capital": "Dover"
-}, {
-    "name": "Florida",
-    "capital": "Tallahassee"
-}, {
-    "name": "Georgia",
-    "capital": "Atlanta"
-}, {
-    "name": "Hawaii",
-    "capital": "Honolulu"
-}, {
-    "name": "Idaho",
-    "capital": "Boise"
-}, {
-    "name": "Illinois",
-    "capital": "Springfield"
-}, {
-    "name": "Indiana",
-    "capital": "Indianapolis"
-}, {
-    "name": "Iowa",
-    "capital": "Des Moines"
-}, {
-    "name": "Kansas",
-    "capital": "Topeka"
-}, {
-    "name": "Kentucky",
-    "capital": "Frankfort"
-}, {
-    "name": "Louisiana",
-    "capital": "Baton Rouge"
-}, {
-    "name": "Maine",
-    "capital": "Augusta"
-}, {
-    "name": "Maryland",
-    "capital": "Annapolis"
-}, {
-    "name": "Massachusetts",
-    "capital": "Boston"
-}, {
-    "name": "Michigan",
-    "capital": "Lansing"
-}, {
-    "name": "Minnesota",
-    "capital": "St. Paul"
-}, {
-    "name": "Mississippi",
-    "capital": "Jackson"
-}, {
-    "name": "Missouri",
-    "capital": "Jefferson City"
-}, {
-    "name": "Montana",
-    "capital": "Helena"
-}, {
-    "name": "Nebraska",
-    "capital": "Lincoln"
-}, {
-    "name": "Nevada",
-    "capital": "Carson City"
-}, {
-    "name": "New Hampshire",
-    "capital": "Concord"
-}, {
-    "name": "New Jersey",
-    "capital": "Trenton"
-}, {
-    "name": "New Mexico",
-    "capital": "Santa Fe"
-}, {
-    "name": "New York",
-    "capital": "Albany"
-}, {
-    "name": "North Carolina",
-    "capital": "Raleigh"
-}, {
-    "name": "North Dakota",
-    "capital": "Bismarck"
-}, {
-    "name": "Ohio",
-    "capital": "Columbus"
-}, {
-    "name": "Oklahoma",
-    "capital": "Oklahoma City"
-}, {
-    "name": "Oregon",
-    "capital": "Salem"
-}, {
-    "name": "Pennsylvania",
-    "capital": "Harrisburg"
-}, {
-    "name": "Rhode Island",
-    "capital": "Providence"
-}, {
-    "name": "South Carolina",
-    "capital": "Columbia"
-}, {
-    "name": "South Dakota",
-    "capital": "Pierre"
-}, {
-    "name": "Tennessee",
-    "capital": "Nashville"
-}, {
-    "name": "Texas",
-    "capital": "Austin"
-}, {
-    "name": "Utah",
-    "capital": "Salt Lake City"
-}, {
-    "name": "Vermont",
-    "capital": "Montpelier"
-}, {
-    "name": "Virginia",
-    "capital": "Richmond"
-}, {
-    "name": "Washington",
-    "capital": "Olympia"
-}, {
-    "name": "West Virginia",
-    "capital": "Charleston"
-}, {
-    "name": "Wisconsin",
-    "capital": "Madison"
-}, {
-    "name": "Wyoming",
-    "capital": "Cheyenne"
 }]
+
+# print a welcome prompt
+print('Welcome to State Capitals! You will be state, you are to guess its capital!')
+
+# this holds our state and capital, it prompts the user to guess the capital of that state, after being looped in the guess_capital function
+def check_answer(state):
+    capital = state["capital"]
+    guess = input("What is the capital of " + state["name"] + "? ")
+    if guess.lower() == capital.lower():
+        print("Correct! You get the worlds best cookie")
+        return True
+    else:
+        print("Incorrect. The capital of " + state["name"] + " is " + capital + ". You get no cookie :(")
+        return False
+    
+# define a recursive function, that prints a shuffled state
+# i want to make the game case recursive, so that when a user wants to play again, the states are shuffled again
+def guess_capital():
+    # random was imported at the time, call the shuffle function on the states list
+    random.shuffle(states)
+    # define counter variables
+    correct_guesses = 0
+    incorrect_guesses = 0
+    # loop through each list in state
+    for state in states:
+        # conditional to add the counter variables, if answer is correct: add to correct, and vice versa
+        if check_answer(state):
+            correct_guesses += 1
+        else:
+            incorrect_guesses += 1
+    # after every state in the list has been guessed, end the game, and output the counter variables
+    print("Game over! You guessed", correct_guesses, "capitals correct and", incorrect_guesses, "capitals incorrect.")
+    # include a play_again function that prompts user to play again if desired
+    # if yes, call guess_capital recursively 
+    play_again = input("Wanna try again? (y/n)").lower()
+    if play_again == "y":
+        guess_capital()
+    # if no, thank user for playing
+    elif play_again == "n":
+        print("Goodbye, Thanks for playing!")
+    # if the input is not y or n, output an error message and provide the correct input
+    else:
+        print("Invalid input. Your choices were (Y or N)")
+        guess_capital()
+
+guess_capital()
